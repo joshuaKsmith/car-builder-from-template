287f6d64035ce01cd6d06e879f094b88e001a0e3
Joshua Smith <mtgmirage@protonmail.com>
2025-01-08 10:53:52 -0600

    refactored app to use calculated properties in data structure instead of handling business logic on front-end

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,36 +1,24 @@
-import { getWheels, getInteriors, getTechnologies, getPaints, getOrders } from "./database.js"
-
-
-const paints = await getPaints()
-const interiors = await getInteriors()
-const techs = await getTechnologies()
-const wheels = await getWheels()
+import { getOrders } from "./database.js"
 
 export const Orders = async () => {
     const orders = await getOrders()
 
-    return `${
-        orders.map(order => {
-            const paint = paints.find(p => p.id === order.paintColorId)
-            const technology = techs.find(t => t.id === order.technologyId)
-            const interior = interiors.find(i => i.id === order.interiorId)
-            const wheel = wheels.find(w => w.id === order.wheelsId)
-
-            return `<section class="order">
-                ${paint.color} car with
-                ${wheel.style} wheels,
-                ${interior.material} interior,
-                and the ${technology.package}
-                for a total cost of
-                ${
-                    (paint.price + technology.price + interior.price + wheel.price).toLocaleString("en-US", {
+    return `
+        ${orders.map(order => {
+            return `
+                <section class="order">
+                    ${order.paintColor.color} car with
+                    ${order.wheels.style} wheels,
+                    ${order.interior.material} interior,
+                    and the ${order.technology.package}
+                    for a total cost of
+                    ${(order.paintColor.price + order.technology.price + order.interior.price + order.wheels.price).toLocaleString("en-US", {
                         style: "currency",
                         currency: "USD"
-                    })
-                }
-            </section>`
-        })
-        .join("")
-    }`
+                    })}
+                </section>
+            `
+        }).join("")}
+    `
 }
 
